0e5a54550e1e2cb51e5bf9671049fd4dfabb611d
Renzo Jared <rleon@globalrisk.pe>
2022-04-29 16:39:22 -0500

    ++ entrie DataContext

--- Baalaven/Baalaven.Entities/POCOEntities/Product.cs
+++ Baalaven/Baalaven.Entities/POCOEntities/Product.cs
@@ -8,7 +8,7 @@ namespace Baalaven.Entities.POCOEntities
 {
     public class Product
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
---
+++ Baalaven/Baalaven.Repositories.EFCore/ContextData/BaalavenContext.cs
@@ -0,0 +1,81 @@
+﻿using Baalaven.Entities.POCOEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Repositories.EFCore.ContextData
+{
+    public class BaalavenContext : DbContext
+    {
+        public BaalavenContext(DbContextOptions<BaalavenContext> options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products  { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating( ModelBuilder modelBuilder )
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Id)
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Customer>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(40);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.CustomerId)
+                .IsRequired()
+                .HasMaxLength(5)
+                .IsFixedLength();
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipAddres)
+                .IsRequired()
+                .HasMaxLength(60);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCity)                
+                .HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipCountry)
+                .HasMaxLength(15);
+            modelBuilder.Entity<Order>()
+                .Property(o => o.ShipPostalCode)
+                .HasMaxLength(10);
+
+            modelBuilder.Entity<OrderDetail>()
+                .HasKey(od => new { od.OrderId, od.ProducId });
+
+            modelBuilder.Entity<Order>()
+                .HasOne<Customer>()
+                .WithMany()
+                .HasForeignKey(o => o.CustomerId);
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(od => od.ProducId);
+
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product { Id = 1, Name = "Chai" },
+                    new Product { Id = 2, Name = "Chang" },
+                    new Product { Id = 3, Name = "Aniseed Syrup" }
+                );
+
+            modelBuilder.Entity<Customer>()
+                .HasData(
+                new Customer { Id = "ALFKI", Name = "Alfreds F." },
+                new Customer { Id = "ANATR", Name = "Ana Trujillo" },
+                new Customer { Id = "ANTON", Name = "Antonio Moreno" }
+                );
+
+        }
+
+    }
+}
