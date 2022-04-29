497bb022a104f72df1062aba61dc631e4e3d3c88
Renzo Jared <rleon@globalrisk.pe>
2022-04-29 13:10:40 -0500

    Add exceptions class

---
+++ Baalaven/Baalaven.Entities/Exceptions/GeneralException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Entities.Exceptions
+{
+    public class GeneralException : Exception
+    {
+        public string Detail { get; set; }
+        public GeneralException() { }
+        public GeneralException(string message) : base(message) { }
+        public GeneralException(string message, Exception innerException) : base(message, innerException) { }
+        public GeneralException(string tittle, string detail) : base(tittle) => Detail = detail;
+
+    }
+}
