a6d9c8e1093e6f4667af80c6c301f5ac50b6bffa
Renzo Jared <rleon@globalrisk.pe>
2022-05-02 13:01:20 -0500

    ++ Entities initial interfaces

---
+++ Baalaven/Baalaven.Entities/Interfaces/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using Baalaven.Entities.POCOEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Entities.Interfaces
+{
+    public interface IOrderDetailRepository
+    {
+        void Create(OrderDetail orderDetail);
+    }
+}
---
+++ Baalaven/Baalaven.Entities/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Baalaven.Entities.POCOEntities;
+using Baalaven.Entities.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Entities.Interfaces
+{
+    public interface IOrderRepository
+    {
+        void Create(Order order);
+        IEnumerable<Order> GetOrdersBySpecification(Specification<Order> specification);
+    }
+}
