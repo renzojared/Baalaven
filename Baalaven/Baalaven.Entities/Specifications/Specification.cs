87ca5caf07f8267926005237a550b6121c7814ab
Renzo Jared <rleon@globalrisk.pe>
2022-05-02 12:53:16 -0500

    Specifications++

---
+++ Baalaven/Baalaven.Entities/Specifications/Specification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Entities.Specifications
+{
+    public abstract class Specification<T>
+    {
+        public abstract Expression<Func<T, bool>> Expression { get; }
+        public bool ISSatisfiedBy(T entity)
+        {
+            Func<T, bool> ExpressionDelegate = Expression.Compile();
+            return ExpressionDelegate(entity);
+        }
+    }
+}
