3c89c8aec403565b7df912db788e5102f69513b6
Renzo Jared <rleon@globalrisk.pe>
2022-05-02 13:19:08 -0500

    ++ DTOs Uses Cases

---
+++ Baalaven/Baalaven.UseCasesDTOs/CreateOrder/CreateOrderDetailParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCasesDTOs.CreateOrder
+{
+    public class CreateOrderDetailParams
+    {
+        public int ProducId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+    }
+}
---
+++ Baalaven/Baalaven.UseCasesDTOs/CreateOrder/CreateOrderParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCasesDTOs.CreateOrder
+{
+    public class CreateOrderParams
+    {
+        public string CustomerId { get; set; }
+        public string ShippAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipCountry { get; set; }
+        public string ShipPostalCode { get; set; }
+        public List<CreateOrderDetailParams> OrderDetails { get; set; }
+    }
+}
