34611aa66507721f83e615fe3ec88f76412fd837
Renzo Jared <rleon@globalrisk.pe>
2022-05-09 15:27:32 -0500

    Add Interfaces Input & Output Ports

---
+++ Baalaven/Baalaven.UseCases/Common/Ports/IInputPort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCases.Common.Ports
+{
+    public interface IInputPort<InteractorRequestType, InteractorResponseType>
+    {
+        public InteractorRequestType RequestData { get; }
+        public IOutputPort<InteractorResponseType> OutputPort { get; }
+    }
+}
---
+++ Baalaven/Baalaven.UseCases/Common/Ports/IOutputPort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCases.Common.Ports
+{
+    public interface IOutputPort<InteractorResponseType>
+    {
+        void Handle(InteractorResponseType response);
+    }
+}
