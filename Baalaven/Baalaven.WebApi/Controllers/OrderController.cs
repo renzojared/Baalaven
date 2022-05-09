d75697cb32b81c9083db803d8ad7d536edfe51df
Jared <renzojared_lm@hotmail.com>
2022-05-09 01:48:54 -0500

    Add Controller Order to Web Api

---
+++ Baalaven/Baalaven.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Baalaven.UsesCases.CreateOrder;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Baalaven.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        readonly IMediator Mediator;
+        public OrderController(IMediator mediator) => Mediator = mediator;
+        [HttpPost("create-order")]
+        public async Task<ActionResult<int>> CreateOrder(
+            CreateOrderInputPort orderparams)
+        {
+            return await Mediator.Send(orderparams);
+        }
+    }
+}
