8d87ae5f318c4a1ba3fe5d1bb39b6341255464ea
Jared <renzojared_lm@hotmail.com>
2022-05-09 00:31:31 -0500

    ++Validation Behavior

---
+++ Baalaven/Baalaven.UsesCases/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCases.Common.Behaviors
+{
+    public  class ValidationBehavior<TRequest, TResponse>: 
+        IPipelineBehavior<TRequest, TResponse> 
+        where TRequest : IRequest<TResponse>
+    {
+        readonly IEnumerable<IValidator<TRequest>> Validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => Validators = validators;
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var Failures = Validators
+                .Select(v => v.Validate(request))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+            if (Failures.Any())
+            {
+                throw new ValidationException(Failures);                
+            }
+            return next();
+        }
+    }
+}
