8d55ab3c71a9df1246c6ea0a7b20afd9ba6913bd
Jared <renzojared_lm@hotmail.com>
2022-05-15 21:47:09 -0500

    Restructuring CreateOrderInteractor

--- Baalaven/Baalaven.UseCases/CreateOrder/CreateOrderInteractor.cs
+++ Baalaven/Baalaven.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -1,51 +1,51 @@
 ﻿using Baalaven.Entities.Exceptions;
 using Baalaven.Entities.Interfaces;
 using Baalaven.Entities.POCOEntities;
-using MediatR;
+using Baalaven.UseCasesDTOs.CreateOrder;
+using Baalaven.UseCasesPorts.CreateOrder;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Baalaven.UseCases.CreateOrder
 {
-    public class CreateOrderInteractor : AsyncRequestHandler<CreateOrderInputPort>
+    public class CreateOrderInteractor : ICreateOrderInputPort
     {
         readonly IOrderRepository OrderRepository;
         readonly IOrderDetailRepository OrderDetailRepository;
         readonly IUnitOfWork UnitOfWork;
+        readonly ICreateOrderOutputPort OutputPort;
 
         public CreateOrderInteractor(IOrderRepository orderRepository,
             IOrderDetailRepository orderDetailRepository,
-            IUnitOfWork unitOfWork) =>
-            (OrderRepository, OrderDetailRepository, UnitOfWork) =
-            (orderRepository, orderDetailRepository, unitOfWork);
-        protected async override Task Handle(CreateOrderInputPort request, CancellationToken cancellationToken)
+            IUnitOfWork unitOfWork,
+            ICreateOrderOutputPort outputPort) =>
+            (OrderRepository, OrderDetailRepository, UnitOfWork, OutputPort) =
+            (orderRepository, orderDetailRepository, unitOfWork, outputPort);
+
+        public async Task Handle(CreateOrderParams order)
         {
             Order Order = new Order
             {
-                CustomerId      = request.RequestData.CustomerId,
-                OrderDate       = DateTime.Now,
-                ShipAddres      = request.RequestData.ShippAddress,
-                ShipCity        = request.RequestData.ShipCity,
-                ShipCountry     = request.RequestData.ShipCountry,
-                ShipPostalCode  = request.RequestData.ShipPostalCode,
-                ShippingType    = Entities.Enums.ShippingType.Road,
-                DiscountType    = Entities.Enums.DiscountType.Percentage,
-                Discount        = 10
+                CustomerId = order.CustomerId,
+                OrderDate = DateTime.Now,
+                ShipAddres = order.ShippAddress,
+                ShipCity = order.ShipCity,
+                ShipCountry = order.ShipCountry,
+                ShipPostalCode = order.ShipPostalCode,
+                ShippingType = Entities.Enums.ShippingType.Road,
+                DiscountType = Entities.Enums.DiscountType.Percentage,
+                Discount = 10
             };
             OrderRepository.Create(Order);
-            foreach (var Item in request.RequestData.OrderDetails)
+            foreach (var Item in order.OrderDetails)
             {
                 OrderDetailRepository.Create(
                     new OrderDetail
                     {
-                        Order       = Order,
-                        ProducId    = Item.ProducId,
-                        UnitPrice   = Item.UnitPrice,
-                        Quantity    = Item.Quantity
+                        Order = Order,
+                        ProducId = Item.ProducId,
+                        UnitPrice = Item.UnitPrice,
+                        Quantity = Item.Quantity
                     });
             }
             try
@@ -56,7 +56,7 @@ namespace Baalaven.UseCases.CreateOrder
             {
                 throw new GeneralException("Error al crear la orden.", ex.Message);
             }
-            request.OutputPort.Handle(Order.Id);
+            await OutputPort.Handle(Order.Id);
         }
     }
 }
