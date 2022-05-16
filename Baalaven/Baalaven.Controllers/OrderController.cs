10736afae478ef1406cb60b4606075fac7cf4e5e
Jared <renzojared_lm@hotmail.com>
2022-05-15 19:31:13 -0500

    Create Order Controller

---
+++ Baalaven/Baalaven.Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Baalaven.Presenters;
+using Baalaven.UseCases.CreateOrder;
+using Baalaven.UseCasesDTOs.CreateOrder;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Baalaven.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController
+    {
+        readonly IMediator Mediator;
+        public OrderController(IMediator mediator) => Mediator = mediator;
+        
+        [HttpPost("create-order")]
+        public async Task<string> CreateOrder(CreateOrderParams orderparams)
+        {
+            CreateOrderPresenter Presenter = new CreateOrderPresenter();
+            await Mediator.Send(new CreateOrderInputPort(orderparams, Presenter));
+            return Presenter.Content;
+        }
+    }
+}
