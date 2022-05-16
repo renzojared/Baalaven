01a12a8c486154bb72c56f08f662e5b1c9733804
Jared <renzojared_lm@hotmail.com>
2022-05-15 22:39:36 -0500

    Delete folder Behavior and Ports. ++ Validator.cs

---
+++ Baalaven/Baalaven.UseCases/Common/Validators/Validator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCases.Common.Validators
+{
+    public class Validator<Model>
+    {
+        public Task<List<ValidationFailure>> Validate(Model model, IEnumerable<IValidator<Model>> validators, bool causesException = true)
+        {
+            var Failures = validators
+                .Select(v => v.Validate(model))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+            if (Failures.Any() && causesException)
+            {
+                throw new ValidationException(Failures);
+            }
+            return Task.FromResult(Failures);
+        }
+    }
+}
