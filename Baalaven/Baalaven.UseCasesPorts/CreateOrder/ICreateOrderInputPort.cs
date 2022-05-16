ac355681b4257247574a27e0879c7a2ac33d7771
Jared <renzojared_lm@hotmail.com>
2022-05-15 21:30:29 -0500

    Create OrderInputPort

---
+++ Baalaven/Baalaven.UseCasesPorts/CreateOrder/ICreateOrderInputPort.cs
@@ -0,0 +1,10 @@
+﻿using Baalaven.UseCasesDTOs.CreateOrder;
+using System.Threading.Tasks;
+
+namespace Baalaven.UseCasesPorts.CreateOrder
+{
+    public interface ICreateOrderInputPort
+    {
+        Task Handle(CreateOrderParams order);
+    }
+}
