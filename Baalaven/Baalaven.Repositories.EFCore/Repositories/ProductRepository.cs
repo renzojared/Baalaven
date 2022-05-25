02325876d600ef4d3199fb169b56eae5f1b5a1fc
Jared <renzojared_lm@hotmail.com>
2022-05-25 10:26:28 -0500

    Add Product Repository

---
+++ Baalaven/Baalaven.Repositories.EFCore/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Baalaven.Entities.Interfaces;
+using Baalaven.Entities.POCOEntities;
+using Baalaven.Entities.Specifications;
+using Baalaven.Repositories.EFCore.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baalaven.Repositories.EFCore.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        readonly BaalavenContext context;
+
+        public ProductRepository(BaalavenContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Product> GetProductsByEspecification(Specification<Product> specification)
+        {
+            var expressionDelegate = specification.Expression.Compile();
+            return context.Products.Where(expressionDelegate);
+        }
+    }
+}
