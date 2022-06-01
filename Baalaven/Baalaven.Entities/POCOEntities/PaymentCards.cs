3d36329418fc4bcc54066021099883c810ee7d51
Renzo Jared <rleon@globalrisk.pe>
2022-06-01 16:43:42 -0500

    ++ Payment Entities & Enums

---
+++ Baalaven/Baalaven.Entities/POCOEntities/PaymentCards.cs
@@ -0,0 +1,14 @@
+﻿using Baalaven.Entities.Enums;
+
+namespace Baalaven.Entities.POCOEntities
+{
+    public class PaymentCards
+    {
+        public int Id { get; set; }
+        public CardType CardType { get; set; }
+        public string CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpireDate { get; set; }
+        public int CVVData { get; set; }
+    }
+}
---
+++ Baalaven/Baalaven.Entities/POCOEntities/PaymentDetails.cs
@@ -0,0 +1,15 @@
+﻿using Baalaven.Entities.Enums;
+using System;
+
+namespace Baalaven.Entities.POCOEntities
+{
+    public class PaymentDetails
+    {
+        public int IdPaymentDetails { get; set; }
+        public int IdPayment { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public int IdPaymentCard { get; set; }
+    }
+}
