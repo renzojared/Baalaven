54a6545660b32397715ae534dc750a649ccf3612
Renzo Jared <renzojared_lm@hotmail.com>
2022-07-18 00:40:55 -0500

    Add to see exceptions and reconfig password

--- Baalaven/Baalaven.Repositories.EFCore/DataContext/BaalavenContextFactory.cs
+++ Baalaven/Baalaven.Repositories.EFCore/DataContext/BaalavenContextFactory.cs
@@ -15,13 +15,18 @@ namespace Baalaven.Repositories.EFCore.DataContext
             var OptionBuilder = new DbContextOptionsBuilder<BaalavenContext>();
             //OptionBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;database=BaalavenDB"); //mssqllocaldb: connection name
 
-            OptionBuilder.UseSqlServer("Server = localhost; Database = BaalavenDB; User Id = sa; Password = rleon@TPA09"); // for docker container
+            OptionBuilder.UseSqlServer("Server = localhost; Database = BaalavenDB; User Id = sa; Password = docker@TPA09"); // for docker container
             
             return new BaalavenContext(OptionBuilder.Options);
             //add-migration InitialCreate -p Baalaven.Repositories.EFCore -c BaalavenContext -o Migrations -s Baalaven.Repositories.EFCore
             // -p : Default project // -c : Context data // -o : place folder // -s : initial project or tools to start
             //update-database -p Baalaven.Repositories.EFCore -s Baalaven.Repositories.EFCore
             //remove - migration - p Baalaven.Repositories.EFCore - s Baalaven.Repositories.EFCore
+            /*
+             See errors:
+                https://localhost:{PortNo}/swagger/v1/swagger.json
+                https://stackoverflow.com/questions/56859604/swagger-not-loading-failed-to-load-api-definition-fetch-error-undefined
+             */
         }
     }
 }
