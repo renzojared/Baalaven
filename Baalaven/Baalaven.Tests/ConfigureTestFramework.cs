d0ccc274dcf2759ce68eb526983472c5f5fd35c3
Renzo Jared <rleon@globalrisk.pe>
2022-07-20 18:17:54 -0500

    Try unit test, pending resolve conflicts

---
+++ Baalaven/Baalaven.Tests/ConfigureTestFramework.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Baalaven.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit.Frameworks.Autofac;
+
+namespace Baalaven.Tests
+{
+    public class ConfigureTestFramework : AutofacTestFramework
+    {
+        public ConfigureTestFramework(IMessageSink diagnosticMessageSink) : base(diagnosticMessageSink)
+        {
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            //builder.RegisterModule<DependencyContainer>();
+        }
+    }
+}
---
+++ Baalaven/Baalaven.Tests/GetAllOrders/GetAllOrdersTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Baalaven.Entities.Interfaces;
+using Baalaven.Controllers;
+using Baalaven.Repositories.EFCore.Repositories;
+using Baalaven.Entities.Specifications;
+using Baalaven.Entities.POCOEntities;
+using Microsoft.AspNetCore.Mvc;
+using Baalaven.UseCasesPorts.CreateOrder;
+using Xunit.Frameworks.Autofac;
+
+namespace Baalaven.Tests.GetAllOrders
+{
+    [UseAutofacTestFramework]
+    public class GetAllOrdersTests 
+    {
+        private readonly IOrderRepository OrderRepository;
+
+        public GetAllOrdersTests(IOrderRepository orderRepository)
+        {
+            OrderRepository = orderRepository;
+        }
+
+        [Fact]
+        public void Get_AllOrdersRepository()
+        {
+            Order order = new Order();            
+            order.Id = 1;
+
+            var customer = new Specification<Order>(o => o.Id == order.Id);
+            var abc = OrderRepository.GetOrdersBySpecification(customer);
+
+            Assert.Equal(1, 1);
+            //Assert.IsType<IEnumerable<Order>>(abc);
+            //Assert.Throws<NullReferenceException>(() => OrderRepository.GetOrdersBySpecification(customer));
+        }
+    }
+}
