7ba51b7fc23eddc33b77d1d5aa31b6d3419d30e4
Renzo Jared <rleon@globalrisk.pe>
2022-07-25 11:15:06 -0500

    Probando ando

---
+++ Baalaven/Baalaven.Tests/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.DependencyInjection;
+
+using Baalaven.IoC;
+using Baalaven.WebExceptionsPresenter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+[assembly: TestFramework("Baalaven.WebApi.Startup", "Baalaven.Tests")]
+
+namespace Baalaven.Tests
+{
+    public class Startup : DependencyInjectionTestFramework
+    {
+        public Startup(IMessageSink messageSink) : base(messageSink)
+        {
+        }
+
+        //Override the method of createhostbuilder to register the configuration and service here
+        protected override IHostBuilder CreateHostBuilder(AssemblyName assemblyName)
+        {
+            var hostBuilder = base.CreateHostBuilder(assemblyName);
+            hostBuilder
+                //Registration configuration
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder
+                        .AddInMemoryCollection(new Dictionary()
+                        {
+                            {"UserName", "Alice"}
+                        })
+                        .AddJsonFile("appsettings.json")
+                        ;
+                })
+                //Register custom service
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton();
+                    if (context.Configuration.GetAppSetting("XxxEnabled"))
+                    {
+                        services.AddSingleton();
+                    }
+                })
+                ;
+
+            return hostBuilder;
+        }
+
+        protected override void Configure(IServiceProvider provider)
+        {
+            //Some test data that needs to be initialized can be put here
+        }
+    }
+}
---
+++ Baalaven/Baalaven.UTest/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using AspectCore.Configuration;
+using AspectCore.DependencyInjection;
+using AspectCore.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Baalaven.IoC;
+
+namespace Baalaven.UTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IHostBuilder CreateHostBuilder()
+        {
+            return new HostBuilder()
+                .ConfigureAppConfiguration(builder =>
+                {                    
+                    builder
+                        .AddInMemoryCollection(new Dictionary<string, string>()
+                        {
+                            {"UserName", "Alice"}
+                        })
+                        .AddJsonFile("appsettings.json")
+                        ;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddBaalavenServices(Configuration);
+                })
+                ;
+        }        
+    }
+}
